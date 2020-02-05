79644b7b3f5bbdf2216bae5165eb57d00fa764af
ShinyQ_11 <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2020-02-05 15:02:58 +0700

    ADD | My Single Linked List Practice

---
+++ Practice/SingleLinkedList/hello.h
@@ -0,0 +1,22 @@
+/*
+    File hello.h
+    Name : Kurniadi Ahmad Wijaya
+    NIM : 1301194024
+*/
+
+#ifndef HELLO_H_INCLUDED
+#define HELLO_H_INCLUDED
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void greetings(string , string );
+void swap_1(int a, int b, int c);
+void swap_2(int a, int &b, int &c);
+void confused();
+void remove_inside(int);
+string indexing(double, double, double);
+
+#endif // HELLO_H_INCLUDED
